28af05d5e926f8aeaade3389b96de551635b4073
RichardENeumann <RichardNeumann@googlemail.com>
2024-05-14 17:59:05 +0200

    Add search function

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-import { key } from "./key.js";
+import { giphyKey } from "./key.js";
 
 const img = document.querySelector("img");
-fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=cat`, {mode: "cors"})
-  .then((response) => {
-    return response.json();
-  })
-  .then((response) => {
-    img.src = response.data.images.original.url;
-  })
+const inpSearch = document.getElementById("inpSearch");
+
+function fetchGIF() {
+
+  const search = encodeURI(inpSearch.value);
+
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyKey}&s=${search}`, {mode: "cors"})
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+      if (response.meta.status != 200) {
+        console.log(response.meta.msg);
+      } else {
+        img.src = response.data.images.original.url;
+      }
+    });
+}
+
+document.getElementById("btSearch").addEventListener("click", fetchGIF);
