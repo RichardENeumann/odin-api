bd2d0825655bc44e66ebde601201eeac808af642
RichardENeumann <RichardNeumann@googlemail.com>
2024-05-14 18:29:14 +0200

    Refactor to use async/await

--- main.js
+++ main.js
@@ -3,24 +3,20 @@ import { giphyKey } from "./key.js";
 const img = document.querySelector("img");
 const inpSearch = document.getElementById("inpSearch");
 
-function fetchGIF() {
-
+async function fetchGIF() {
   const search = encodeURI(inpSearch.value);
-
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyKey}&s=${search}`, {mode: "cors"})
-    .catch ((response) => {
-      console.log(response.meta.msg);
-    })
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-      if (response.data.length == 0) {
-        console.log(response.meta.msg);
-      } else {
-        img.src = response.data.images.original.url;
-      }
-    });
+  
+  try {
+    let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyKey}&s=${search}`, {mode: "cors"});
+    let parsedResponse = await response.json();
+    if (parsedResponse.data.length === 0) {
+      console.log("Found nothing, server status: ", parsedResponse.meta.msg);
+    } else {
+      img.src = parsedResponse.data.images.original.url;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 document.getElementById("btSearch").addEventListener("click", fetchGIF);
